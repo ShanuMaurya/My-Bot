d38c65dab8502b1fd3d0c13292a905dcfd50ddd8
ShanuMaurya <36756132+ShanuMaurya@users.noreply.github.com>
2018-06-19 11:27:15 +0530

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,48 @@
+const Discord = require("discord.js");
+
+const config = require("./config.json");
+const prefix = require("./config.json");
+
+var fortunes = [
+             "idk",
+             "maybe",
+             "wtf",
+             "what are u asking?"
+             ];
+             
+var bot = new Discord.Client();
+
+bot.on("Ready", function() {
+	        console.log("Ready");
+	});
+
+bot.on("message", function(message) {
+	if (message.author.equals(bot.user)) return; 
+	
+	If (!message.content.startswith(prefix)) return; 
+	
+	var args = message.content.substring(prefix.length).split("  ");
+   
+   switch (args[0].toLowerCase()) {
+   	case "ping":
+                  message.channel.sendMessage("pong!");
+                  break; 
+       case "info":
+                  message. channel.sendMessage("I'm a special bot made by WolfBoy(melo)");
+                  break;
+       case "8ball":
+                  If (args[1]) message.channel.sendMessage(fortunes[Math.floor(Math.random()  *  fortunes.length)]);
+                  else message.channel.sendMessage("I can't read that question sorry");
+                  else message.channel.sendMessage("Try something else please");
+                  break; 
+               
+       default:
+                 message.channel.sendMessage("Please read the commands and info to use me");
+                 
+                 }
+                 
+            });
+            
+            
+         bot.login(config.token);
+           
